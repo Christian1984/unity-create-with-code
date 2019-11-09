ccc03d10d7299fb8cc108cfd92d86dd23b174c77
Christian Hoffmann <chris.hoffmann84@gmail.com>
2019-11-09 22:00:24 +0100

    added towercontroller

---
+++ Lab - CubeDefender/Assets/Scripts/TowerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerController : MonoBehaviour
+{
+    [SerializeField] private float range = 0;
+    [SerializeField] private float cadence = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject closest = GetClosestEnemy();
+
+        if (closest && Vector3.Distance(closest.transform.position, transform.position) <= range)
+        {
+            AttackTarget(closest);
+        }
+    }
+
+    private GameObject GetClosestEnemy()
+    {
+        //TODO
+        return GameObject.FindGameObjectWithTag("Enemy");
+    }
+
+    private void AttackTarget(GameObject target)
+    {
+        Debug.DrawLine(transform.position, target.transform.position);
+    }
+}
